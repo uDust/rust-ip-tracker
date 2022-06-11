342b3579965c6e0e37974808d72e04bf5cfe1405
Omar <whoisomar@protonmail.com>
2022-06-11 16:48:19 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+extern crate reqwest;
+
+#[tokio::main]
+async fn main() {
+    let mut api = String::from("http://ip-api.com/json/");
+    let mut ip = String::new();
+    
+
+    //get a user input
+    println!("introduce una ip: ");
+    let _bytes = io::stdin()
+        .read_line(&mut ip)
+        .unwrap();
+    
+    api.push_str(&ip);
+
+
+    let response = reqwest::get(api)
+        .await
+        .unwrap()
+        .text()
+        .await;
+    println!("{:?}", response);
+
+    
+}
