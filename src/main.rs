1c39f20148382e8c41b14b68c51f214b6b1a4f93
Omar <whoisomar@protonmail.com>
2022-06-11 23:39:48 -0500

    new images

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
-//this is a work in progress
-//esto es un trabajo en progreso
-//:)
+/*    crates and libraries    */ 
 use std::io;
+use std::process;
+extern crate serde;
 extern crate reqwest;
 
+/*  main function  */
 #[tokio::main]
 async fn main() {
+    //variables
     let mut api = String::from("http://ip-api.com/json/");
     let mut ip = String::new();
-    
+   
+    //structure from ip information
+    #[derive(serde::Deserialize)]
+    struct IpInf {
+        query: String,
+        status: String,
+        country: String,
+        countryCode: String,
+        region: String,
+        regionName: String,
+        city: String,
+        zip: String,
+        lat: f32,
+        lon: f32,
+        timezone: String,
+        isp: String,
+        org: String,
+    }
 
     //get a user input
     println!("introduce una ip: ");
@@ -18,13 +37,26 @@ async fn main() {
     
     api.push_str(&ip);
 
-
-    let response = reqwest::get(api)
+    //request
+    let info = reqwest::get(api)
         .await
         .unwrap()
-        .text()
-        .await;
-    println!("{:?}", response);
-
+        .json::<IpInf>()
+        .await
+        .unwrap();
     
-}
+    //print info 
+    println!("\n\n\n\n-----------------------------------------");
+    println!("target: {:#?}", info.query);
+    println!("country: {:#?}", info.country);
+    println!("country code: {:#?}", info.countryCode);
+    println!("region: {:#?}", info.region);
+    println!("region name: {:#?}", info.regionName);
+    println!("city: {:#?}", info.city);
+    println!("zip code: {:#?}", info.zip);
+    println!("latitude: {:#?}", info.lat);
+    println!("longitude: {:#?}", info.lon);
+    println!("timezone: {:#?}", info.timezone);
+    println!("isp: {:#?}", info.isp);
+    println!("org: {:#?}", info.org);
+} 
